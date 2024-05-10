10d6b9bfee0592b664787f015c6e2b923e846645
Kenneth S. Reyes <kenneth.rejes@gmail.com>
2024-05-10 08:43:34 +0200

    Make metadata external

---
+++ R/countrynames.R
@@ -0,0 +1,11 @@
+#' List of countries
+#'
+#' \describe{
+#'   \item{iso3n, iso2c, iso3c}{ISO country codes}
+#'   \item{eurostat, wb}{Other country codes}
+#'   \item{unname}{Officially recognized name}
+#'   \item{unname_sort}{Name used for sorting}
+#'   \item{popname}{Popular name}
+#'   \item{region}{World Bank region}
+#' }
+"countrynames"
