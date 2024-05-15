b56a71c4a4c376dc5c1439391569e175de241a32
Kenneth S. Reyes <kenneth.rejes@gmail.com>
2024-05-15 18:11:18 +0200

    Update data cleaning scripts

--- data-raw/create_datasets.R
+++ data-raw/create_datasets.R
@@ -1,4 +1,6 @@
 
+library(tidyverse)
+
 pivot_years <- function(data) {
   tidyr::pivot_longer(
     data,
@@ -37,47 +39,7 @@ stocks <- dplyr::bind_rows(
   dplyr::select(from = orig, to = dest, sex, t, v) |>
   dplyr::arrange(from, to, sex, t)
 
-# usethis::use_data(stocks, overwrite = TRUE)
-
-
-# Indicators --------------------------------------------------------------
-
-varlist <- tibble::tribble(
-  ~code,      ~file,
-  "pop",      "Population",
-  "nmig",     "NetMigration",
-  "remin",    "RemittancesReceived",
-  "remout",   "RemittancesPaid",
-  "fdiin",    "FDIinflow",
-  "fdiout",   "FDIoutflow",
-  "birth",    "BirthRate",
-  "depd",     "AgeDependency",
-  "inc",      "Income",
-  "unem",     "Unemployment",
-  "inf",      "Inflation",
-  "grieve",   "GroupGrievance",
-  "polstb",   "PoliticalStabilityNoViolence"
-)
-
-add_table <- function(code, file) {
-  readr::read_csv(stringr::str_glue("data-raw/raw/{file}.csv")) |>
-    pivot_years() |>
-    dplyr::mutate(var = code) |>
-    dplyr::select(iso = CountryCode, var, t, v) |>
-    tidyr::drop_na()
-}
-
-indicators <- tibble::tibble()
-
-for (i in 1:nrow(varlist)) {
-  indicator_i <- add_table(code = varlist[i, ]$code, file = varlist[i, ]$file)
-  indicators <- dplyr::bind_rows(indicators, indicator_i)
-}
-
-indicators <- dplyr::arrange(indicators, iso, var, t)
-
-# usethis::use_data(indicators, overwrite = TRUE)
-
+usethis::use_data(stocks, overwrite = TRUE)
 
 # Disasters ---------------------------------------------------------------
 
@@ -135,38 +97,64 @@ disasters <- dplyr::bind_rows(
 
 # usethis::use_data(disasters, overwrite = TRUE)
 
-
 # IDMC --------------------------------------------------------------------
 
-# Displacements by country and by coordinates
-
-idmc1 <- "IDMC_Internal_Displacement_Conflict-Violence_Disasters.xlsx"
-# idmc2 <- "IDMC_GIDD_Internal_Displacement_Disaggregated.xlsx"
-idmc2 <- "Displacements_geolocated.csv"
-
-idmc_nat <- readxl::read_excel(stringr::str_glue("data-raw/raw/{idmc1}")) |>
-  dplyr::select(
-    -tidyselect::ends_with("(Raw)"),
-    -tidyselect::ends_with("Stock Displacement")
+file <- "IDMC_Internal_Displacement_Conflict-Violence_Disasters.xlsx"
+idmc <- readxl::read_excel(stringr::str_glue("data-raw/{file}")) |>
+  select(
+    -ends_with("(Raw)"),
+    -ends_with("Stock Displacement")
   ) |>
-  tidyr::pivot_longer(
-    cols = tidyselect::ends_with("Displacements"),
+  pivot_longer(
+    cols = ends_with("Displacements"),
     names_to = "category",
     names_pattern = "([A-Za-z]+) Internal Displacements",
     values_to = "v"
   ) |>
-  tidyr::replace_na(list(v = 0)) |>
-  dplyr::select(iso = ISO3, category, t = Year, v) |>
-  dplyr::mutate(
-    dataset = "national",
+  replace_na(list(v = 0)) |>
+  select(iso = ISO3, category, t = Year, v) |>
+  mutate(
     t = as.integer(t),
     longitude = NA,
     latitude = NA,
     v = as.integer(v)
-  )
+  ) |>
+  select(iso, category, t, v) |>
+  arrange(iso, category, t)
 
-idmc_geo <- readr::read_csv(stringr::str_glue("data-raw/raw/{idmc2}")) |>
-  dplyr::select(
+usethis::use_data(idmc, overwrite = TRUE)
+
+# WPP ---------------------------------------------------------------------
+
+wpp <- readr::read_csv(
+  "data-raw/WPP2022_PopulationBySingleAgeSex_Medium_1950-2021.csv",
+  col_types = list(Time = "integer", AgeGrp = "integer")
+) |>
+  select(
+    iso = ISO3_code,
+    t = Time,
+    age = AgeGrp,
+    Male = PopMale,
+    Female = PopFemale,
+    Total = PopTotal
+  ) |>
+  pivot_longer(
+    cols = c(Male, Female, Total),
+    names_to = "sex",
+    values_to = "v"
+  ) |>
+  drop_na() |>
+  filter(t %in% c(seq(1950, 2020, 10), 2021)) |>
+  select(iso, t, sex, age, v) |>
+  arrange(iso, t, sex, age)
+
+usethis::use_data(wpp, overwrite = TRUE)
+
+# Internal ----------------------------------------------------------------
+
+file <- "Displacements_geolocated.csv"
+idmc_geo <- readr::read_csv(stringr::str_glue("data-raw/raw/{file}")) |>
+  select(
     iso = CountryCode,
     category = type,
     type = category,
@@ -174,8 +162,7 @@ idmc_geo <- readr::read_csv(stringr::str_glue("data-raw/raw/{idmc2}")) |>
     longitude, latitude,
     v = displacements
   ) |>
-  dplyr::mutate(
-    dataset = "geo",
+  mutate(
     t = as.integer(t),
     v = as.integer(v),
     type = dplyr::case_when(
@@ -188,57 +175,14 @@ idmc_geo <- readr::read_csv(stringr::str_glue("data-raw/raw/{idmc2}")) |>
       category == "Other" ~ "Conflict",
       .default = category
     )
-  )
-
-idmc <- dplyr::bind_rows(idmc_nat, idmc_geo) |>
-  dplyr::select(dataset, iso, category, type, t, longitude, latitude, v) |>
-  dplyr::arrange(dataset, iso, category, type, t)
-
-# usethis::use_data(idmc, overwrite = TRUE)
-
-
-# WPP ---------------------------------------------------------------------
-
-# Population shares by age and sex from UN World Population Prospects
-
-wpp <- readr::read_csv(
-  "data-raw/raw/WPP2022_PopulationBySingleAgeSex_Medium_Percentage_1950-2021.csv",
-  col_types = list(Time = "integer", AgeGrp = "integer")
-) |>
-  dplyr::select(
-    iso = ISO3_code,
-    t = Time,
-    age = AgeGrp,
-    Male = PopMale,
-    Female = PopFemale,
-    Total = PopTotal
   ) |>
-  tidyr::pivot_longer(
-    cols = c(Male, Female, Total),
-    names_to = "sex",
-    values_to = "v"
-  ) |>
-  tidyr::drop_na() |>
-  dplyr::filter(t == 2021) |>
-  dplyr::select(iso, t, sex, age, v) |>
-  dplyr::arrange(iso, t, sex, age)
-
-# usethis::use_data(wpp, overwrite = TRUE)
-
-
-
-# Captions ----------------------------------------------------------------
+  arrange(iso, category, type, t)
 
 captions <- readr::read_csv("data-raw/captions.csv")
 
-# usethis::use_data(captions, overwrite = TRUE)
-
 usethis::use_data(
-  stocks,
-  indicators,
   disasters,
-  idmc,
-  wpp,
+  idmc_geo,
   captions,
   internal = TRUE,
   overwrite = TRUE
---
+++ data-raw/create_indicators.R
@@ -0,0 +1,133 @@
+
+library(tidyverse)
+
+# WDI ---------------------------------------------------------------------
+
+wbcountries <- read_csv("data-raw/Metadata_Country.csv") |>
+  select(`Country Code`, Region, TableName) |>
+  drop_na() |>
+  full_join(countrynames, by = c("Country Code" = "iso3c")) |>
+  select(`Country Code`, TableName) |>
+  drop_na() |>
+  pull(`Country Code`) |>
+  tolower() |>
+  paste(collapse = ";")
+
+wdi_codes <- tribble(
+  ~code,                  ~var,
+  "SP.POP.TOTL",          "pop",
+  "SM.POP.NETM",          "nmig",
+  "NY.GDP.PCAP.PP.KD",    "income",
+  "SL.UEM.TOTL.ZS",       "unem",
+  "FP.CPI.TOTL.ZG",       "inf",
+  "BM.TRF.PWKR.CD.DT",    "remout",
+  "BX.TRF.PWKR.CD.DT",    "remin",
+  "BX.KLT.DINV.CD.WD",    "fdiin",
+  "BM.KLT.DINV.CD.WD",    "fdiout",
+  "SP.DYN.CBRT.IN",       "birth",
+  "SP.POP.DPND",          "depend"
+)
+
+build_url <- function(code) {
+  paste0(
+    "http://api.worldbank.org/v2/country/",
+    wbcountries,
+    "/indicator/",
+    code,
+    "?format=json&date=1990:2050&per_page=9999"
+  )
+}
+
+wdi <- tibble()
+
+for (i in 1:nrow(wdi_codes)) {
+
+  wdi_i <- jsonlite::fromJSON(build_url(wdi_codes$code[i])) |>
+    as.data.frame() |>
+    mutate(var = wdi_codes$var[i]) |>
+    select(iso = countryiso3code, var, t = date, v = value) |>
+    drop_na()
+
+  wdi <- bind_rows(wdi, wdi_i)
+}
+
+# FSI ---------------------------------------------------------------------
+
+fsi_files <- list.files("data-raw/fragile_states_index")
+
+fsi <- tibble()
+
+for (file in fsi_files) {
+
+  fsi_i <- readxl::read_excel(
+    paste0("data-raw/fragile_states_index/", file)
+  ) |>
+    mutate(
+      t = str_extract(file, "(\\d){4}"),
+      var = "grieve",
+      Country = case_when(
+        Country == "Israel and West Bank" ~ "Israel",
+        Country == "Micronesia" ~ "Federated States of Micronesia",
+        .default = Country
+      ),
+      iso = countrycode::countrycode(
+        Country,
+        origin = "country.name",
+        destination = "iso3c"
+      )) |>
+    select(iso, var, t, v = `C3: Group Grievance`)
+
+  fsi <- bind_rows(fsi, fsi_i)
+}
+
+# WGI ---------------------------------------------------------------------
+
+cols <- readxl::read_excel(
+  "data-raw/wgidataset.xlsx",
+  sheet = "Political StabilityNoViolence",
+  skip = 13,
+  col_names = FALSE,
+  n_max = 2
+)
+
+colnames <- c(
+  as.vector(unlist(cols[2, 1:2])),
+  paste0(unlist(cols[1, -(1:2)]), "_", unlist(cols[2, -(1:2)]))
+)
+
+wgi <- readxl::read_excel(
+  "data-raw/wgidataset.xlsx",
+  sheet = "Political StabilityNoViolence",
+  skip = 15,
+  col_names = colnames,
+  na = "#N/A"
+) |>
+  pivot_longer(
+    cols = -c(`Country/Territory`, Code),
+    names_to = c("t", "measure"),
+    names_sep = "_",
+    values_to = "v"
+  ) |>
+  filter(measure == "Estimate") |>
+  mutate(
+    iso = case_when(
+      Code == "ADO" ~ "AND",
+      Code == "KSV" ~ "XKX",
+      Code == "WBG" ~ "PSE",
+      Code == "ROM" ~ "ROU",
+      Code == "TMP" ~ "TLS",
+      Code == "ZAR" ~ "COD",
+      .default = Code
+    ),
+    var = "polstb",
+  ) |>
+  drop_na() |>
+  select(iso, var, t, v)
+
+# Consolidate -------------------------------------------------------------
+
+indicators <- bind_rows(wdi, fsi, wgi) |>
+  mutate(t = as.integer(t)) |>
+  arrange(iso, var, t)
+
+usethis::use_data(indicators, overwrite = TRUE)
