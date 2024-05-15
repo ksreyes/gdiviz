2d109e6d668240715f1363440a415d4dbb1db7e4
Kenneth S. Reyes <kenneth.rejes@gmail.com>
2024-05-15 18:12:23 +0200

    Update metadata

--- R/gdidata.R
+++ R/gdidata.R
@@ -1,4 +1,5 @@
 #' List of datasets
 #'
-#' All available datasets.
+#' This lists all the datasets bundled with `gdiviz`. `indicators` groups
+#' together all single-variable time series.
 "gdidata"
