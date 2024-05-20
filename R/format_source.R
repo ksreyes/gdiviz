f8265c83ea29a3f53e28b10fd383857e0949b5f9
Kenneth S. Reyes <kenneth.rejes@gmail.com>
2024-05-20 10:45:04 +0200

    Add `color` argument to `format_source()`

--- R/format_source.R
+++ R/format_source.R
@@ -3,14 +3,17 @@
 #' @param source String of the plot's source.
 #' @param basesize Base text size of plot.
 #' @param space_after Whether to add line breaks after the source. Use when plot has a caption.
+#' @param color Text color.
 #'
 #' @return String.
 #'
 #' @export
-format_source <- function(source, basesize = 8, space_after = TRUE) {
+format_source <- function(source,
+                          basesize = 8,
+                          space_after = TRUE,
+                          color = "#8099D0") {
 
   size <- sizer(basesize)$footnote
-  color <- pal("blues", 3)
   style <- stringr::str_glue("font-size:{size}pt; color:{color}")
 
   spaces <- ifelse(space_after, "<br><br>", "")
